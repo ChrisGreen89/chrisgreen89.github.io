7ede387df304ee5dd13afea3ffd6c6b55c041eb9
Chris Green <chris@analogit.com>
2019-05-31 12:23:09 -0600

    Changed form fields

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -78,14 +78,11 @@ class Contact extends Component {
                           data-error="Please specify your need."
                         >
                           <option value="" />
-                          <option value="Request quotation">
-                            Request quotation
+                          <option value="New Website">
+                            New Website (from scratch!)
                           </option>
-                          <option value="Request order status">
-                            Request order status
-                          </option>
-                          <option value="Request copy of an invoice">
-                            Request copy of an invoice
+                          <option value="Website Re-Design">
+                            Website Re-Design
                           </option>
                           <option value="Other">Other</option>
                         </select>
